1ececa41affc2b4da846f6cbb4410f3d0ea61aab
peter.jp.xie <peter.jp.xie@gmail.com>
2020-07-13 08:43:17 +1000

    Daily update

--- dl.py
+++ dl.py
@@ -5,7 +5,7 @@ import time
 from render import render
 
 DOWNLOAD_WAIT = 60 * 2
-RENDER_WAIT = 60 * 3
+RENDER_WAIT = DOWNLOAD_WAIT
 
 def bash(cmd):
     """ bash   
@@ -22,21 +22,23 @@ dl_list = 'download_list.txt'
 f_st = os.stat(dl_list)
 
 # if download_list is modified recently, N minutes
+# print("Current time:%s, dl_list time:%s, delta:%s" %(time.time(),f_st.st_mtime,time.time()-f_st.st_mtime))
 if int(time.time() - f_st.st_mtime) < DOWNLOAD_WAIT : 
     with open(dl_list) as f:
         # os.system('cd video')
         for l in f:
-            if l.strip() != '':
-                print('Downloading %s' % l, end='')
+            if l.strip() != '' and not l.startswith('#'):
+                print('Downloading %s' % l, end='',flush=True)
                 r = bash('youtube-dl %s' % l)
                 if r.returncode == 0:
-                    print('File downloaded.')
+                    print('File downloaded.',flush=True)
         
     bash('mv *.mp4 video/')
     bash('mv *.webm video/')
     print('Download done!')
 
 f_st = os.stat('video')
+# print("Current time:%s, video time:%s, delta:%s" %(time.time(),f_st.st_mtime,time.time()-f_st.st_mtime))
 # if download_list is modified recently, N minutes
 if int(time.time() - f_st.st_mtime) < RENDER_WAIT : 
     render()
--- dl.sh
+++ dl.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 if ! ps -ef|grep -v grep | grep dl.py; then
-python /root/Website/dl.py  | tee /root/Website/dl.log  
-fi 
+echo "starting dl.py"
+python /root/Website/dl.py  | tee -a /root/Website/dl.log  
+fi
+echo "dl.sh done" 
